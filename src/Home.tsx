a289447b42be8e281e841f31f288955b1a68f42a
nassro <jokerkadi97@gmail.com>
2026-02-02 00:07:51 +0100

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,8 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { PRODUCTS, BRAND_SLOGAN } from '../constants';
-import ProductCard from '../components/ProductCard';
+import { PRODUCTS, BRAND_SLOGAN } from './constants';
+import ProductCard from './components/ProductCard';
 
 const Home: React.FC = () => {
   const featuredProducts = PRODUCTS.slice(0, 3);
