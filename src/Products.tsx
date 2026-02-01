d47144a4cca2dc271ee6e46323f3d63391e3c698
nassro <jokerkadi97@gmail.com>
2026-02-02 00:09:12 +0100

    Update Products.tsx

--- src/Products.tsx
+++ src/Products.tsx
@@ -1,7 +1,7 @@
 
 import React, { useState } from 'react';
-import { PRODUCTS } from '../constants';
-import ProductCard from '../components/ProductCard';
+import { PRODUCTS } from './constants';
+import ProductCard from './components/ProductCard';
 
 const Products: React.FC = () => {
   const [filter, setFilter] = useState<'all' | 'truffles' | 'bars' | 'collections'>('all');
