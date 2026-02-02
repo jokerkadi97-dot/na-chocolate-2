b1f3feca23aad7f5a1539c457d5349d029dc1405
nassro <jokerkadi97@gmail.com>
2026-02-02 13:10:01 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,9 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'https://esm.sh/react@18.2.0';
+import ReactDOM from 'https://esm.sh/react-dom@18.2.0/client';
+import App from './App.tsx';
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(React.createElement(App));
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
